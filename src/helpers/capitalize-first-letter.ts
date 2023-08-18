6fcc8ea12f9899f7ee53b29571c201febf4bd0c7
skobets <SergeyKobets@hollandandbarrett.com>
2023-08-18 06:14:37 +0300

    Updates

--- src/helpers/capitalize-first-letter.ts
+++
@@ -1,7 +0,0 @@
-export function capitalizeFirstLetter(word: string): string {
-  if (!word) {
-    return "";
-  }
-
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
--- src/pages/home.tsx
+++
@@ -1,1 +0,0 @@
-export { People as Home } from "../components";
